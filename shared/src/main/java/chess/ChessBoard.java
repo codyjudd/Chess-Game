0bcee0a4cbf4d6c08f83d39cfdec4901ea742ab5
Cody Judd <codymanjudd@gmail.com>
2026-05-13 00:53:41 -0600

    started the basic skelton of phase 1

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,43 +1,85 @@
 package chess;
 
-/**
- * A chessboard that can hold and rearrange chess pieces.
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
- */
+import java.util.Arrays;
+
+
+
 public class ChessBoard {
 
+    private ChessPiece [][] board;
+
     public ChessBoard() {
-        
+        board = new ChessPiece[8][8];
     }
 
-    /**
-     * Adds a chess piece to the chessboard
-     *
-     * @param position where to add the piece to
-     * @param piece    the piece to add
-     */
+
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        int row = position.getRow() -1;
+        int col = position.getColumn() -1;
+
+        board[row][col] = piece;
     }
 
-    /**
-     * Gets a chess piece on the chessboard
-     *
-     * @param position The position to get the piece from
-     * @return Either the piece at the position, or null if no piece is at that
-     * position
-     */
+
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        int row = position.getRow() -1;
+        int col = position.getColumn() -1;
+
+        return board[row][col];
     }
 
-    /**
-     * Sets the board to the default starting board
-     * (How the game of chess normally starts)
-     */
+
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        board = new ChessPiece[8][8];
+
+        ChessPiece.PieceType[] order = {
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK,
+        };
+
+        for (int col =1; col <= 8; col++) {
+            addPiece(new ChessPosition(1, col),
+                    new ChessPiece(ChessGame.TeamColor.WHITE, order[col -1]));
+
+            addPiece(new ChessPosition(2, col),
+                    new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN));
+
+            addPiece( new ChessPosition(7, col),
+                    new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN));
+
+            addPiece(new ChessPosition(8, col),
+                    new ChessPiece(ChessGame.TeamColor.BLACK, order[col- 1]));
+
+        }
     }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ChessBoard)) {
+            return false;
+        }
+
+        ChessBoard other = (ChessBoard) o;
+        return Arrays.deepEquals(board, other.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.deepToString(board);
+    }
+
 }
--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,29 +1,45 @@
 package chess;
 
-/**
- * Represents a single square position on a chess board
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
- */
+import java.util.Objects;
+
 public class ChessPosition {
 
+    private int row;
+    private int col;
+
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
-    /**
-     * @return which row this position is in
-     * 1 codes for the bottom row
-     */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
-    /**
-     * @return which column this position is in
-     * 1 codes for the left column
-     */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
+    }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof ChessPosition)) {
+            return false;
+        }
+
+        ChessPosition other = (ChessPosition) o;
+        return row == other.row && col == other.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row,col);
+    }
+    @Override
+    public String toString() {
+        return "(" + row + ", " + col + ")";
     }
 }
